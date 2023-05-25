25da35b465405f0aaa74dc98347486e0f9e9b33e
leedongyeop <ldy_1204@naver.com>
2023-05-25 14:22:57 +0900

    feat: HttpServletRequest, HttpServletResponse practice

---
+++ servlet/src/test/java/mvcv1/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package mvcv1.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
