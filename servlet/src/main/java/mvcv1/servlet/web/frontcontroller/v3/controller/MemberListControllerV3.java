88df706b7d1c0f8ad34deb78707848a0b3ffba8e
leedongyeop <ldy_1204@naver.com>
2023-05-31 18:03:23 +0900

    feat: 직접 만들어보는 MVC

---
+++ servlet/src/main/java/mvcv1/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -0,0 +1,25 @@
+package mvcv1.servlet.web.frontcontroller.v3.controller;
+
+import mvcv1.servlet.domain.member.Member;
+import mvcv1.servlet.domain.member.MemberRepository;
+import mvcv1.servlet.web.frontcontroller.ModelView;
+import mvcv1.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+
+        List<Member> members = memberRepository.findAll();
+
+        ModelView mv = new ModelView("members");
+        mv.getModel().put("members", members);
+
+        return mv;
+    }
+}
