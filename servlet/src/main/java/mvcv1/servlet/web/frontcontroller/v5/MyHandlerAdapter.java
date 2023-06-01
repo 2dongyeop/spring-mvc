b42bcf0ddad70163ab3f8375f0743067cdbd43c6
leedongyeop <ldy_1204@naver.com>
2023-06-01 15:53:02 +0900

    feat: 핸들러 적용을 통해 컨트롤러 유연성 제공

---
+++ servlet/src/main/java/mvcv1/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,18 @@
+package mvcv1.servlet.web.frontcontroller.v5;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mvcv1.servlet.web.frontcontroller.ModelView;
+
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+
+    boolean supports(Object handler);
+
+    ModelView handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler) throws ServletException, IOException;
+}
