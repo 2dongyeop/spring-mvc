a762ed31d5f6c84ca0c21b58b9e2b9cb2159f726
leedongyeop <ldy_1204@naver.com>
2023-06-14 10:34:52 +0900

    feat: HTTP 응답

---
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,36 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseView1()  {
+
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "response/hello";
+    }
+
+
+    /**
+     * 아래는 권장하지 않는 방법.
+     * v2를 하자.그냥..
+     * @param model
+     */
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!!");
+    }
+}
